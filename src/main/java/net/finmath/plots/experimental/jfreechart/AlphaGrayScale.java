2b784aff3bc1b3fe0b80c573dd52b1c135fefc23
Christian Fries <email@christian-fries.de>
2018-05-24 10:22:34 +0200

    Added two PaintScales for contour plots.

---
+++ src/main/java/net/finmath/plots/experimental/jfreechart/AlphaGrayScale.java
@@ -0,0 +1,37 @@
+package net.finmath.plots.experimental.jfreechart;
+
+
+import java.awt.Color;
+import java.awt.Paint;
+
+import org.jfree.chart.renderer.PaintScale;
+
+public class AlphaGrayScale implements PaintScale {
+    double lowerBound;
+    double upperBound;
+    double frequency;
+    double thickness;
+
+    public AlphaGrayScale(double lowerBound, double upperBound, double frequency, double thickness) {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        this.frequency = frequency;
+        this.thickness = thickness;
+    };
+
+    public double getLowerBound() {
+        return lowerBound;
+    }
+
+    public double getUpperBound() {
+        return upperBound;
+    }
+
+    public Paint getPaint(double value) {
+        final float level = (float)Math.sin(0.5*Math.PI*(1.0-thickness));
+        
+    	if(Double.isNaN(value)) return Color.GRAY;
+    	double percentageValue = Math.min(Math.max((value-lowerBound) / (upperBound-lowerBound),0.0),1.0);
+    	return new Color(0.0f, 0.0f, 0.0f, (float)Math.max(Math.sin(10*2*Math.PI*percentageValue)-level,0.0)/(1.0f-level));
+    }
+}
---
+++ src/main/java/net/finmath/plots/experimental/jfreechart/HuePaintScale.java
@@ -0,0 +1,30 @@
+package net.finmath.plots.experimental.jfreechart;
+
+
+import java.awt.Color;
+import java.awt.Paint;
+
+import org.jfree.chart.renderer.PaintScale;
+
+public class HuePaintScale implements PaintScale {
+    double lowerBound;
+    double upperBound;
+
+    public HuePaintScale(double lowerBound, double upperBound) {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+    };
+
+    public double getLowerBound() {
+        return lowerBound;
+    }
+
+    public double getUpperBound() {
+        return upperBound;
+    }
+
+    public Paint getPaint(double value) {    	
+    	if(value > upperBound || value < lowerBound || Double.isNaN(value)) return Color.GRAY;
+    	return Color.getHSBColor((float) ((value-lowerBound) / (upperBound-lowerBound) * 240.0/360.0), 1.0f, 1.0f);
+    }
+}
