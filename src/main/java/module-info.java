293eaa88ae7170c216763b856620a33b6c211d03
Christian Fries <email@christian-fries.de>
2020-01-31 10:22:40 +0100

    Added Java module file.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+
+module net.finmath.plots {
+
+	exports net.finmath.plots;
+	
+	requires finmath.lib;
+
+	requires jfreechart;
+	requires jfreesvg;
+	requires jzy3d.api;
+	requires itextpdf;
+	requires commons.math3;
+	requires jogl.all;
+
+	requires com.google.common;
+
+	requires javafx.controls;
+	requires javafx.base;
+	requires javafx.graphics;
+	requires javafx.swing;
+
+	requires java.desktop;
+	requires java.management;
+	requires java.logging;
+}
