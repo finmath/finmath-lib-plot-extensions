e6454b51fff04a16dce5112987d8d01d24b61798
Christian Fries <email@christian-fries.de>
2020-06-02 13:56:58 +0200

    Adding commons-lang and jzy-javafx.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,14 @@ module net.finmath.plots {
 
 	exports net.finmath.plots;
 	exports net.finmath.plots.axis;
-
+	exports net.finmath.plots.demo;
+	
 	requires transitive net.finmath.lib;
 
 	requires jfreechart;
 	requires jfreesvg;
 	requires jzy3d.api;
+	requires jzy3d.javafx;
 	requires itextpdf;
 	requires commons.math3;
 	requires jogl.all.main;
@@ -23,4 +25,5 @@ module net.finmath.plots {
 	requires java.desktop;
 	requires java.management;
 	requires java.logging;
+	requires org.apache.commons.lang3;
 }
