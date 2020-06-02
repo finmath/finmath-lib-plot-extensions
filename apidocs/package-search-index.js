9fb37e22613864036f8731b2496dacbd220883ca
Christian Fries <email@christian-fries.de>
2020-06-02 18:20:57 +0200

    Site checkin for project finmath lib plot extensions

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"net.finmath.plots","l":"net.finmath.plots"},{"m":"net.finmath.plots","l":"net.finmath.plots.axis"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"net.finmath.plots","l":"net.finmath.plots"},{"m":"net.finmath.plots","l":"net.finmath.plots.axis"},{"m":"net.finmath.plots","l":"net.finmath.plots.demo"}]
