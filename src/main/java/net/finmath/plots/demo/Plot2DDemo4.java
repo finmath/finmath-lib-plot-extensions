174f7592c03d9a0aea1c89010018bf2e5121dd60
Christian Fries <email@christian-fries.de>
2020-07-28 09:41:19 +0200

    Reworked example

--- src/main/java/net/finmath/plots/demo/Plot2DDemo4.java
+++ src/main/java/net/finmath/plots/demo/Plot2DDemo4.java
@@ -6,8 +6,10 @@
 
 package net.finmath.plots.demo;
 
+import java.io.File;
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.DoubleUnaryOperator;
 
 import net.finmath.plots.Named;
@@ -21,9 +23,12 @@ import net.finmath.plots.Plot2D;
  */
 public class Plot2DDemo4 {
 
+	private static final int numberOfPointsToPlot = 200;
+	private static final boolean isSavePlotsToFile = true;
+
 	/**
 	 * Run the demo.
-	 * Plots the value of an option under the Black-Scholes model as a function of strike and time-to-maturity.
+	 * Plots the value of a finite difference approximation of the derivative of (exp(x)-x) at 0.
 	 *
 	 * @param args Not used.
 	 * @throws Exception Exception from the graphics backend.
@@ -32,10 +37,10 @@ public class Plot2DDemo4 {
 
 		final double x			= 0.0;
 
-		final DoubleUnaryOperator functionDerivativeCentral = (shiftScale) -> {
+		final DoubleUnaryOperator functionDerivativeCentralError = (shiftScale) -> {
 			final double shiftSize = Math.pow(10, -shiftScale);	// shift = 10^{-scale}
 
-			final double valueUp		= Math.exp(x + shiftSize);
+			final double valueUp	= Math.exp(x + shiftSize);
 			final double valueDown	= Math.exp(x - shiftSize);
 
 			final double derivativeFiniteDifference = (valueUp-valueDown) / 2 / shiftSize - 1;
@@ -43,10 +48,10 @@ public class Plot2DDemo4 {
 			return derivativeFiniteDifference;
 		};
 
-		final DoubleUnaryOperator functionDerivativeForward = (shiftScale) -> {
+		final DoubleUnaryOperator functionDerivativeForwardError = (shiftScale) -> {
 			final double shiftSize = Math.pow(10, -shiftScale);	// shift = 10^{-scale}
 
-			final double valueUp		= Math.exp(x + shiftSize);
+			final double valueUp	= Math.exp(x + shiftSize);
 			final double value		= Math.exp(x);
 
 			final double derivativeFiniteDifference = (valueUp-value) / shiftSize - 1;
@@ -59,27 +64,63 @@ public class Plot2DDemo4 {
 			return Math.exp(x) - 1;
 		};
 
-		final Plot plotDerivativeForward = new Plot2D(5, 10.0, 100, Arrays.asList(
-				new Named<DoubleUnaryOperator>("Finite Difference Approximation", functionDerivativeForward),
+		final Plot plotDerivativeForward = new Plot2D(1, 15.0, numberOfPointsToPlot, List.of(
+				new Named<DoubleUnaryOperator>("Finite Difference Approximation", functionDerivativeForwardError),
 				new Named<DoubleUnaryOperator>("Analytic", functionDerivativeAnalytic)));
-		plotDerivativeForward.setTitle("(One Sided Finite Difference) Derivative of exp(x) at x = " + x).setXAxisLabel("scale (h = 10^{-scale})").setYAxisLabel("value").setIsLegendVisible(true);
+		plotDerivativeForward.setTitle("(One Sided Finite Difference) Derivative of exp(x) at x = " + x)
+		.setXAxisLabel("scale (h = 10^{-scale})")
+		.setYAxisLabel("error (approx. value - 1.0)")
+		.setIsLegendVisible(true);
 		((Plot2D)plotDerivativeForward).setYAxisNumberFormat(new DecimalFormat("0.0E00"));
 		plotDerivativeForward.show();
+		if(isSavePlotsToFile) plotDerivativeForward.saveAsPDF(new File("exp-x-forward-fd-large.pdf"), 900, 600);
 
-		final Plot plotDerivativeCentral1 = new Plot2D(4.5, 6.5, 100, Arrays.asList(
-				new Named<DoubleUnaryOperator>("Finite Difference Approximation", functionDerivativeCentral),
+		final Plot plotDerivativeForward3 = new Plot2D(14.0, 16.5, numberOfPointsToPlot, List.of(
+				new Named<DoubleUnaryOperator>("Finite Difference Approximation", functionDerivativeForwardError),
 				new Named<DoubleUnaryOperator>("Analytic", functionDerivativeAnalytic)));
-		plotDerivativeCentral1.setTitle("(Central Finite Difference) Derivative of exp(x) at x = " + x).setXAxisLabel("scale (h = 10^{-scale})").setYAxisLabel("value").setIsLegendVisible(true);
+		plotDerivativeForward3.setTitle("(One Sided Finite Difference) Derivative of exp(x) at x = " + x)
+		.setXAxisLabel("scale (h = 10^{-scale})")
+		.setYAxisLabel("error (approx. value - 1.0)")
+		.setIsLegendVisible(true);
+		((Plot2D)plotDerivativeForward3).setYAxisNumberFormat(new DecimalFormat("0.0E00"));
+		plotDerivativeForward3.show();
+		if(isSavePlotsToFile) plotDerivativeForward3.saveAsPDF(new File("exp-x-forward-fd-corner.pdf"), 900, 600);
+
+		final Plot plotDerivativeForward2 = new Plot2D(6, 10.0, numberOfPointsToPlot, List.of(
+				new Named<DoubleUnaryOperator>("Finite Difference Approximation", functionDerivativeForwardError),
+				new Named<DoubleUnaryOperator>("Analytic", functionDerivativeAnalytic)));
+		plotDerivativeForward2
+		.setTitle("(One Sided Finite Difference) Derivative of exp(x) at x = " + x)
+		.setXAxisLabel("scale (h = 10^{-scale})")
+		.setYAxisLabel("error (approx. value - 1.0)")
+		.setIsLegendVisible(true);
+		((Plot2D)plotDerivativeForward2).setYAxisNumberFormat(new DecimalFormat("0.0E00"));
+		plotDerivativeForward2.show();
+		if(isSavePlotsToFile) plotDerivativeForward2.saveAsPDF(new File("exp-x-forward-fd-zoom.pdf"), 900, 600);
+
+		final Plot plotDerivativeCentral1 = new Plot2D(1.0, 20.0, numberOfPointsToPlot, List.of(
+				new Named<DoubleUnaryOperator>("Finite Difference Approximation", functionDerivativeCentralError),
+				new Named<DoubleUnaryOperator>("Analytic", functionDerivativeAnalytic)));
+		plotDerivativeCentral1
+		.setTitle("(Central Finite Difference) Derivative of exp(x) at x = " + x)
+		.setXAxisLabel("scale (h = 10^{-scale})")
+		.setYAxisLabel("error (approx. value - 1.0)")
+		.setIsLegendVisible(true);
 		((Plot2D)plotDerivativeCentral1).setYAxisNumberFormat(new DecimalFormat("0.0E00"));
-		((Plot2D)plotDerivativeCentral1).setYRange(-5E-10, 5E-10);
 		plotDerivativeCentral1.show();
+		if(isSavePlotsToFile) plotDerivativeCentral1.saveAsPDF(new File("exp-x-central-fd-large.pdf"), 900, 600);
 
-		final Plot plotDerivativeCentral2 = new Plot2D(1.0, 20.0, 100, Arrays.asList(
-				new Named<DoubleUnaryOperator>("Finite Difference Approximation", functionDerivativeCentral),
+		final Plot plotDerivativeCentral2 = new Plot2D(4, 7, numberOfPointsToPlot, List.of(
+				new Named<DoubleUnaryOperator>("Finite Difference Approximation", functionDerivativeCentralError),
 				new Named<DoubleUnaryOperator>("Analytic", functionDerivativeAnalytic)));
-		plotDerivativeCentral2.setTitle("(Central Finite Difference) Derivative of exp(x) at x = " + x).setXAxisLabel("scale (h = 10^{-scale})").setYAxisLabel("value").setIsLegendVisible(true);
-		((Plot2D)plotDerivativeCentral1).setYAxisNumberFormat(new DecimalFormat("0.0E00"));
+		plotDerivativeCentral2
+		.setTitle("(Central Finite Difference) Derivative of exp(x) at x = " + x)
+		.setXAxisLabel("scale (h = 10^{-scale})")
+		.setYAxisLabel("error (approx. value - 1.0)")
+		.setIsLegendVisible(true);
+		((Plot2D)plotDerivativeCentral2).setYAxisNumberFormat(new DecimalFormat("0.0E00"));
+		((Plot2D)plotDerivativeCentral2).setYRange(-5E-10, 5E-10);
 		plotDerivativeCentral2.show();
-
+		if(isSavePlotsToFile) plotDerivativeCentral2.saveAsPDF(new File("exp-x-central-fd-zoom.pdf"), 900, 600);
 	}
 }
